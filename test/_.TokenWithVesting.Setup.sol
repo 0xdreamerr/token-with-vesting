66685df8c4f7e5260be9ae4ec6fc48d0ddf56665
0xdreamerr <0xdreamerr@gmail.com>
2024-11-25 16:38:48 +0300

    Setup updated

--- test/_.TokenWithVesting.Setup.sol
+++ test/_.TokenWithVesting.Setup.sol
@@ -14,6 +14,14 @@ contract TokenWithVestingSetup is Test {
     string symbol;
     uint256 totalSupply;
 
+    // base options for all vestings
+    uint64 public _amount = 1000;
+    uint64 public _start = 10;
+    uint64 public _cliff = 10;
+    uint64 public _vested = 20;
+    bool public _revokable = true;
+    address public _receiver;
+
     function setUp() public virtual {
         owner = address(0x1);
         userB = address(0x2);
